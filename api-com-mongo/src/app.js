ef3d29e1c3a4bb8341265ec9e7c5d35725f54909
Ananda Souto <88149291+ansndsc@users.noreply.github.com>
2021-11-17 17:16:34 -0300

    adiciona app

---
+++ api-com-mongo/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const db = require("./database/mongoConfig")
+const musicaRoutes = require("./routes/musicaRoutes")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use("/musica", musicaRoutes)
+
+db.connect()
+
+module.exports = app
